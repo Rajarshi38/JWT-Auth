20b49a70f4b7b8a064a545bc215b838582e6464e
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-02-09 23:37:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+//connect to database
+
+mongoose.connect(process.env.DB_CONNECT_URI, { newUrlParser: true }, () =>
+    console.log("connected to db")
+);
+//import router
+const authRouter = require("./routes/auth");
+
+//route middleware
+app.use("/api/users", authRouter);
+
+app.listen(3000, () => console.log("server is running on port 3000"));
