d883b9ac7faae7a981184ec0886253430f04e20e
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-02-10 18:50:54 +0530

    fixed Mongodb (working now)

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 //connect to database
 dotenv.config();
-
-mongoose.connect(process.env.DB_CONNECT_URI, { newUrlParser: true }, () =>
-    console.log("connected to db")
-);
 //import router
 const authRouter = require("./routes/auth");
 
+mongoose.connect(process.env.DB_CONNECT_URI, { useNewUrlParser: true }, () =>
+    console.log("connected to db")
+);
+//Middleware
+app.use(express.json());
+
 //route middleware
-app.use("/api/users", authRouter);
+app.use("/api/user", authRouter);
 
 app.listen(3000, () => console.log("server is running on port 3000"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
+const User = require("../model/User");
+const Joi = require("joi");
 
-router.post("/register", (req, res) => {
-    res.send("register done");
+const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required(),
+    password: Joi.string().min(6).required(),
+};
+
+router.post("/register", async (req, res) => {
+    //Let's validate user before saving it
+    Joi.valid
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        const savedUser = await user.save();
+        console.log(savedUser);
+        res.send(savedUser);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 module.exports = router;
