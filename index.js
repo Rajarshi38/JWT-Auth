1a739878cca9cd33847c0abcbeac446fcf391f9f
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-02-11 12:56:47 +0530

    authenticaion completed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 //import router
 const authRouter = require("./routes/auth");
+const postRouter = require("./routes/posts");
 
 mongoose.connect(process.env.DB_CONNECT_URI, { useNewUrlParser: true }, () =>
     console.log("connected to db")
@@ -16,5 +17,6 @@ app.use(express.json());
 
 //route middleware
 app.use("/api/user", authRouter);
+app.use("/api/posts", postRouter);
 
 app.listen(3000, () => console.log("server is running on port 3000"));
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-function verify(req, res, next) {
+module.exports = function (req, res, next) {
     const token = req.header("auth-token");
+    
     if (!token) return res.status(401).send("Access denied");
     try {
         const verified = jwt.verify(token, process.env.JWT_TOKEN);
         req.user = verified;
+        next();
     } catch (error) {
         req.status(400).send("Invalid Token");
     }
-}
+};
